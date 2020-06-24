5aac084d2fd7c68274b498ff9fea54b0946a1fcb
Freeman-Nkosi <nfmajola98@gmail.com>
2020-06-25 00:57:24 +0200

    added password strength

--- Array Auth/JavaScript.js
+++ Array Auth/JavaScript.js
@@ -21,3 +21,30 @@ function Get(){
 	}
 	
 }
+function a (e){
+	console.log(e)
+}
+
+function passwordS(){
+
+		var pass =document.getElementById("password").value;
+		
+	var bc=document.getElementById("password");
+	if(pass==""){
+		bc.style.backgroundColor=""
+	}
+	
+		//var reg= new RegExp("(([a-z]{1,}).{0,}([A-Z]{1,})(([0-9].{1,}[0-9])|[0-9]{2,})")
+		var reg= new RegExp("^(?=.{7,})(?=.*?[a-z])(?=.*?[A-Z])(?=.*?(([0-9]{2,})|([0-9].{0,}[0-9])))")
+		a(pass)
+		if(pass!=""){
+			if(reg.test(pass)){
+				bc.style.backgroundColor="green"
+			}else{
+				bc.style.backgroundColor="red"
+			}
+	}
+	
+	
+	
+}
